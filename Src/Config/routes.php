27227d6e6f4b30cc23fca7905dbe018e824aa538
benyuxPetr <benyuxp@gmail.com>
2020-02-21 16:20:38 +0300

    Роутер часть 1

---
+++ Src/Config/routes.php
@@ -0,0 +1,3 @@
+<?php
+$this->router->add('main', '/', 'HomeController:index');
+$this->router->add('login', '/registration', 'RegisterController:index');
--- Src/Core/App.php
+++ Src/Core/App.php
@@ -6,13 +6,19 @@ class App
 {
     private $di;
 
+    public $router;
+
     public function __construct($di)
     {
         $this->di = $di;
+        $this->router = $this->di->get('router');
     }
 
     public function run()
     {
+        require_once CONFIG_DIR.'routes.php';
 
+        var_dump($this->router->routes);
+        die();
     }
 }
