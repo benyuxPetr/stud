1b555af4eeadf335edf3d18b0cf044398d9267a5
benyuxPetr <benyuxp@gmail.com>
2020-02-27 18:39:48 +0300

    добавил верстку

--- Public/Templates/home.php
+++ Public/Templates/home.php
@@ -2,5 +2,47 @@
 $data = $this->theme->getData();
 $this->theme->header();
 ?>
-<h1>home</h1>
+    <h1 class="h1 d-flex justify-content-center">Студенты</h1>
+    <div class="d-flex flex-column">
+        <table class="table table-sm">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Имя</th>
+                <th scope="col">Фамилия</th>
+                <th scope="col">Пол</th>
+                <th scope="col">Группа</th>
+                <th scope="col">Email</th>
+                <th scope="col">Баллы ЕГЭ</th>
+                <th scope="col">Дата рождения</th>
+                <th scope="col">Местный/Иногородний</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Dylan</td>
+                <td>Heintze</td>
+                <td>M</td>
+                <td>2J4</td>
+                <td>dheintze0@dagondesig</td>
+                <td>248</td>
+                <td>2008-02-29</td>
+                <td>Иногородний</td>
+            </tr>
+            </tbody>
+        </table>
+        <nav aria-label="...">
+            <ul class="pagination pagination-md justify-content-center">
+                <li class="page-item active" aria-current="page">
+                    <span class="page-link">
+                        1
+                        <span class="sr-only">(current)</span>
+                    </span>
+                </li>
+                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+            </ul>
+        </nav>
+    </div>
 <?php $this->theme->footer(); ?>
--- Public/Templates/register.php
+++ Public/Templates/register.php
@@ -2,5 +2,64 @@
 $data = $this->theme->getData();
 $this->theme->header();
 ?>
-<h1>Register</h1>
+    <h1 class="h1 d-flex justify-content-center">Регистрация</h1>
+    <div class="row justify-content-md-center">
+        <div class="col-md-5">
+        <form>
+            <div class="form-group row">
+                <label for="inputName" class="col-sm-3 col-form-label">Имя</label>
+                <div class="col-sm-9">
+                    <input type="text" class="form-control" name="name" id="inputName" placeholder="Имя">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="inputLastName" class="col-sm-3 col-form-label">Фамилия</label>
+                <div class="col-sm-9">
+                    <input type="text" class="form-control" name="lastName" id="inputLastName" placeholder="Фамилия">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="sex" class="col-sm-3 col-form-label">Пол</label>
+                <div class="col-sm-9">
+                    <select class="form-control" id="sex">
+                        <option value="M">Mужской</option>
+                        <option value="F">Женский</option>
+                    </select>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="inputEmail" class="col-sm-3 col-form-label">Email</label>
+                <div class="col-sm-9">
+                    <input type="email" class="form-control" name="email" id="inputEmail" placeholder="Email">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="inputExam" class="col-sm-3 col-form-label">Баллы ЕГЭ</label>
+                <div class="col-sm-9">
+                    <input type="number" class="form-control" name="exam" id="inputExam" placeholder="Баллы ЕГЭ">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="inputDate" class="col-sm-3 col-form-label">Дата Рождения</label>
+                <div class="col-sm-9">
+                    <input class="form-control" type="date" id="inputDate" placeholder="Дата Рождения">
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-sm-3">Местный</div>
+                <div class="col-sm-9">
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" id="inputCheck" name="resident" value="resident">
+                        <label class="form-check-label" for="inputCheck">Местный</label>
+                    </div>
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-sm-3">
+                    <button type="submit" class="btn btn-primary">Отправить</button>
+                </div>
+            </div>
+        </form>
+        </div>
+    </div>
 <?php $this->theme->footer(); ?>
